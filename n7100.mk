cb7dc94cdbd8480027b417fe55c70eef2420ed7a
sarcastillo <sar.castillo@gmail.com>
2013-04-01 09:06:21 +0200

    HyperDroidAOXP: Enable AMR wideband for better call quality (test)
    Change-Id: Ib50c1af4822bb3fc7b272d7029abff880cba4dc7

--- n7100.mk
+++ n7100.mk
@@ -82,7 +82,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungExynos4RIL \
     mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
     ro.ril.hsxpa=1 \
-    ro.ril.gprsclass=10
+    ro.ril.gprsclass=10 \
+    ro.ril.amr.wideband=1
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
